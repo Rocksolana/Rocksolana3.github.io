2bec41bb03e9f82a921f6bf16ba2811f9988d04a
ВАШЕ_ИМЯ <tsypuk@gmail.com>
2021-01-09 17:32:43 +0200

    added file

---
+++ app.js
@@ -0,0 +1,28 @@
+//Завдання1
+/*let number = prompt('введіть число','');
+number=number*number;
+alert(`${number}`);*/
+
+
+//Завдання2
+/*
+let Name=prompt('введіть імя','');
+let deposit=prompt('введіть суму','');
+let month=prompt('введіть місяць','');
+let percent=0.2;
+summa=(1+percent/12)**month;
+summa=summa*deposit;
+
+alert(`Шановний ${Name} ви внесли суму ${deposit} під ${percent} річних, на термін ${month} місяців. За цей період часу Ви заробите ${summa}`);
+
+*/
+
+//Завдання3
+
+let num1=prompt('введіть число а','');
+let num2=prompt('введіть число b','');
+ num1 = Number(num1);
+ num2 = Number(num2);
+
+
+alert(`Сума ${num1+num2}, Різниця ${num1-num2} , Множення ${num1*num2}, Ділення ${num1/num2} `);
